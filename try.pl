bd7cb93fd34e94bb8dcf2dd285d632c7b8b6b391
Cindy Wang (CindyLinz) <cindylinz@gmail.com>
2014-05-22 13:15:39 +0800

    version dependency perl 5.0182 -> 5.006, add -O2 to CCFLAGS

--- try.pl
+++ try.pl
@@ -3,12 +3,13 @@
 use strict;
 use warnings;
 
+use lib qw(/home/cindy/perl5/lib/perl);
 use Data::Dumper;
 
 use Router::R3;
 
 #Router::R3::test();
-my $t = Router::R3->new({
+my $t = Router::R3->new(
     '/foo/bar' => 2,
     '/zoo' => 1,
     '/bar' => 3,
@@ -16,7 +17,7 @@ my $t = Router::R3->new({
     '/post2/{id:\d+}' => 5,
     '/post3/{idx:\d{3}}' => 6,
     '/post4/{idx:\d{3}}/{idy}' => 7,
-});
+);
 my($a, $b) = $t->match('/post4/333/22');
 print "t=$t, a=$a, b=$b\n";
 if( $b ) {
